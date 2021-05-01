c909a58d0039d69fe84f11972620e5b3fbe45e9e
Andrew Yankee <anjoyank@gmail.com>
2021-05-01 07:25:06 -0400

    wamp a bamp doo daw

--- nodeJsBasics/app.js
+++ nodeJsBasics/app.js
@@ -2,8 +2,40 @@ const http = require('http');
 
 const port = 5050;
 
-const server = http.createServer();
+
+//create server endpoints and clumsily inject data
+//will learn better ways in the future
+const server = http.createServer((req, res) => {
+    switch (req.url) {
+        case '/':
+            res.end(`
+        <html>
+        <head>
+            <title>First node app</title>
+        </head>
+        <body>
+            <h1>Welcome to my node app</h1>
+        </body>
+        </html>
+        `);
+        break;
+        case '/admin':
+            res.end(`
+            welcome to the admin page
+            `);
+        break;
+        case '/user':
+            res.end(`
+            welcome to the user page
+            `);
+        break;
+        default:
+            res.end(`not found`);
+    };
+}
+);
 
 server.listen(port, () => {
-    console.log('server is now listening on port ${port}');
+    console.log(`server is now listening on port ${port}`);
 });
+
