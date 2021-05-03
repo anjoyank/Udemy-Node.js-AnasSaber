fe4b0edbbca66b8256f5fd578c645ba93592c77b
Andrew Yankee <anjoyank@gmail.com>
2021-05-03 16:50:33 -0400

    section 7

--- nodeJsBasics/eLoop.js
+++ nodeJsBasics/eLoop.js
@@ -1,4 +1,5 @@
 const { pbkdf2 } = require('crypto');
+const { createServer } = require('http');
 
 const start = Date.now();
 
@@ -8,7 +9,20 @@ const hash = () => {
     });
 };
 
+const lis = () => {
+    createServer().listen(3000, () => {
+        console.log('listening', Date.now() - start);
+    });
+};
+
+//the hash method runs 4 times, each takes about the same amount of time
+//each hash() gets added to the call stack and the process gets added to libuv
+//if there are more than 4, the extra takes about twice as long
 hash();
 hash();
 hash();
 hash();
+
+//adding this method to the end transfers its call the underlying OS
+//process continues to run bc server is now listening
+lis();
